601339e9d25703691e210d72b3e4a2cbfdc67ed1
shaban <146794146+sarzhh@users.noreply.github.com>
2025-04-08 22:28:00 +0000

    completed linked list

--- index.js
+++ index.js
@@ -44,5 +44,103 @@ export class linkedList{
             current = current.nextNode
         }
         console.log(result+"null");
+        return result;
+    }
+
+    size(){
+        if (this.head== null){
+            console.log("size is 0")
+            return 0;
+        }
+        let current = this.head;
+        let size = 0
+        while(current != null){
+            size++
+            current = current.nextNode;
+        }
+        console.log("size is "+ size)
+        return size;
+    }
+
+    first(){
+        if (this.head== null){
+            console.log("first node is empty")
+        }else{
+            console.log("the first node is "+ this.head.value)
+        }
+    }
+
+    tail(){
+        let current = this.head
+        while(current.nextNode != null){
+            current = current.nextNode
+        }
+        console.log("the last node is "+ current.value)
+    }
+
+    at(index){
+        let position = 0
+        let current = this.head
+        let listSize = this.size() - 1
+
+
+        if (index>listSize || index < 0){
+            console.log("index not available")
+            return
+        }
+
+        while(position!= index){
+            position++;
+            current = current.nextNode
+        }
+        console.log(`the value at ${index} is ${current.value} `)
+    }
+
+    pop(){
+        if (this.head == null) {
+            console.log("List is empty");
+            return;
+        }
+        if (this.head.nextNode == null) {
+            this.head = null;
+            return;
+        }
+
+        let current = this.head;
+        while(current.nextNode && current.nextNode.nextNode!= null){
+            current = current.nextNode
+        }
+        current.nextNode = null
+        
+        this.toString();
+    }
+
+    contains(value){
+        let current = this.head
+
+        while(current != null){
+           if (current.value === value){
+            console.log("true")
+            return true;
+           }
+           current = current.nextNode;
+    } console.log("false")
+    return false;
+    }
+
+    find(value){
+        let index = 0;
+        let current = this.head;
+
+        while(current != null){
+            if (current.value === value){
+                console.log(`${value} is at index ${index}`)
+                return;
+            }else{
+                current= current.nextNode
+                index++
+            }
+        } console.log("not in the list")
+        return null;
     }
 }
--- main.js
+++ main.js
@@ -7,3 +7,10 @@ list.append(10)
 list.preppend(30)
 list.append(40)
 list.toString();
+list.size();
+list.first();
+list.tail();
+list.at(5)
+list.pop();
+list.contains(100)
+list.find(300)
